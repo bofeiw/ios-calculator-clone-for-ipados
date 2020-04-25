8d7558e20f46cb5e30bbdd9a2a075bf42d0ade70
Bofei Wang <secure@bofeiwang.com>
2020-04-25 23:22:46 +1000

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Bofei Wang on 25/4/20.
+//  Copyright © 2020 bofei. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CalculatorTests/CalculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  CalculatorTests.swift
+//  CalculatorTests
+//
+//  Created by Bofei Wang on 25/4/20.
+//  Copyright © 2020 bofei. All rights reserved.
+//
+
+import XCTest
+@testable import Calculator
+
+class CalculatorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
